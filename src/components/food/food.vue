1319c5ea6c19ecded92fff248a8affcc0fc28d4f
9515 <953857170@qq.com>
2017-11-07 22:16:27 +0800

    eleme-app

---
+++ src/components/food/food.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+</template>
+
+<script>
+export default {
+    methods: {
+        show () {
+            
+        }
+    }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
